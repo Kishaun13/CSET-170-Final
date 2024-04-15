ea9b01337e56a10233ce3d5a99fded71270b625f
Kishaun13 <Kishaunbrown14@gmail.com>
2024-04-15 11:27:45 -0400

    add to the html templates and created related app routes

--- main.py
+++ main.py
@@ -18,5 +18,27 @@ def hello():
     return render_template('index.html')
 
 
+@app.route('/login', methods=['GET'])
+def login():
+    return render_template('login.html')
+
+
+@app.route('/login', methods=['POST'])
+def login_post():
+    return render_template('login.html')
+
+
+@app.route('/register', methods=['GET'])
+def register():
+    return render_template('register.html')
+
+
+@app.route('/register', methods=['POST'])
+def register_post():
+    return render_template('register.html')
+
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
