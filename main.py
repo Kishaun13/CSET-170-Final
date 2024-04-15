5f85287985c3b604b05076994a91b48dc27292e6
Kishaun13 <Kishaunbrown14@gmail.com>
2024-04-15 10:36:46 -0400

    added main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template
+from sqlalchemy import create_engine
+
+app = Flask(__name__)
+app.secret_key = 'password123'
+connect = "mysql://root:Applepine13.!@localhost/banking"
+engine = create_engine(connect, echo=True)
+conn = engine.connect()
+
+
+@app.route('/base')
+def add():
+    return render_template('base.html')
+
+
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
