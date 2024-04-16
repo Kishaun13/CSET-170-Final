b07a04d81c475e1b78a821b950649155b0adf390
Kishaun13 <Kishaunbrown14@gmail.com>
2024-04-16 09:03:24 -0400

    created html pages for login along with pages that are opened when logged in

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
-from sqlalchemy import create_engine
+from flask import Flask, render_template, request
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker
 
 app = Flask(__name__)
 app.secret_key = 'password123'
@@ -23,10 +24,37 @@ def login():
     return render_template('login.html')
 
 
+from flask import session as flask_session
+
+
 @app.route('/login', methods=['POST'])
 def login_post():
-    return render_template('login.html')
+    with app.app_context():
+        engine = create_engine(connect)
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        conn = engine.connect()
 
+        user = session.execute(text('SELECT email, acc_type FROM USERS WHERE email = :email AND Keyword = :Keyword'),
+                               request.form).fetchone()
+        session.commit()
+        conn.commit()
+
+        if user:
+            if user.acc_type == 'Admin':
+                admin = session.execute(text('SELECT * FROM admins WHERE email = :email'),
+                                        {'email': user.email}).fetchone()
+                flask_session['user_id'] = admin.AdminID  # Store the admins id in the session
+                return render_template('admin.html')
+            elif user.acc_type == 'Customer':
+                customer = session.execute(text('SELECT * FROM customers WHERE email = :email'),
+                                           {'email': user.email}).fetchone()
+                flask_session['user_id'] = customer.AccountID  # Store the student's ID in the session
+                flask_session['customer_id'] = customer.AccountID  # Store the student's ID in the session
+                return render_template('homepage.html')
+        else:
+            invalid = "Invalid email or password"
+            return render_template('login.html', invalid=invalid)
 
 @app.route('/register', methods=['GET'])
 def register():
@@ -34,10 +62,22 @@ def register():
 
 
 @app.route('/register', methods=['POST'])
-def register_post():
+def create_user():
+    conn.execute(text('INSERT INTO Users (Email, full_name, acc_type, Keyword) VALUES (:email, :full_name, "Customer", :Keyword)'), request.form)
+    conn.commit()
     return render_template('register.html')
 
 
+@app.route('/Customer_create', methods=['GET'])
+def create_request():
+    return render_template('Customer_create.html')
+
+
+@app.route('/Customer_create', methods=['POST'])
+def create_customer():
+    conn.execute(text('INSERT INTO Customers (AccountNumber, CustomerID, SSN, Address, PhoneNumber, Email, Passwords, debit_cardnumber, Card_Pin, Expire_Date) VALUES (:AccountNumber, :CustomerID, :SSN, :Address, :PhoneNumber, :Email, :Passwords, :debit_cardnumber, :Card_Pin, :Expire_Date)'), request.form)
+    conn.commit()
+    return render_template('Customer_create.html')
 
 
 if __name__ == '__main__':
